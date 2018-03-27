19e500f9115dc152b1a979e425f5f864707ff43b
Rayintu <rickybroers.rb@gmail.com>
2018-03-27 16:07:44 +0200

    classes aangemaakt

---
+++ OOPG-ICA-man/src/main/java/nl/han/ica/ICAman/Docent.java
@@ -0,0 +1,13 @@
+package nl.han.ica.ICAman;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithGameObjects;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
+
+import java.util.List;
+
+public class Docent implements ICollidableWithGameObjects {
+    @Override
+    public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
+
+    }
+}
