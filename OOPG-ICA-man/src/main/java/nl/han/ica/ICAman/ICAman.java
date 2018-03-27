5def33ae885fad87dc958b3e70a802c9eb3713c0
Rayintu <rickybroers.rb@gmail.com>
2018-03-27 16:18:22 +0200

    nog een update
    27/03/2018
    16:18

--- OOPG-ICA-man/src/main/java/nl/han/ica/ICAman/ICAman.java
+++ OOPG-ICA-man/src/main/java/nl/han/ica/ICAman/ICAman.java
@@ -1,4 +1,28 @@
 package nl.han.ica.ICAman;
 
 public class ICAman {
+    public static void main(String[] args) {
+
+    }
+
+    public void setupGame(){
+
+    }
+
+    public void createView(int screenWidth,int screenHeight){
+
+    }
+
+    public void initializeSound(){
+
+    }
+
+    public void createObjects(){
+
+    }
+
+    public void editScore(){
+        
+    }
+
 }
