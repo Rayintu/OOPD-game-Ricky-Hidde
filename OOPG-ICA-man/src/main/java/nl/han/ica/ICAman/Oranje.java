270564850d591d441d99c8c0214daaf8fda61b90
Rayintu <rickybroers.rb@gmail.com>
2018-04-03 11:27:53 +0200

    Revert "Revert "HET SCHERM WERKT YEET""
    This reverts commit 7e1bddc5d11aa58e6c054379b89fc8113b41b66d.

---
+++ OOPG-ICA-man/src/main/java/nl/han/ica/ICAman/Oranje.java
@@ -0,0 +1,4 @@
+package nl.han.ica.ICAman;
+
+public class Oranje extends Student {
+}
---
+++ OOPG-ICA-man/src/main/java/nl/han/ica/ICAman/Student.java
@@ -0,0 +1,13 @@
+package nl.han.ica.ICAman;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithGameObjects;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
+
+import java.util.List;
+
+public class Student implements ICollidableWithGameObjects {
+    @Override
+    public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
+
+    }
+}
