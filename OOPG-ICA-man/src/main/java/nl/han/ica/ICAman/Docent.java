ab1836fae6d6fbc0b4916f94825b322eec2e3680
Rayintu <rickybroers.rb@gmail.com>
2018-04-03 11:51:21 +0200

    adfa

---
+++ OOPG-ICA-man/src/main/java/nl/han/ica/ICAman/Docent.java
@@ -0,0 +1,80 @@
+package nl.han.ica.ICAman;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.CollidedTile;
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithGameObjects;
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithTiles;
+import nl.han.ica.OOPDProcessingEngineHAN.Exceptions.TileNotFoundException;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.AnimatedSpriteObject;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import nl.han.ica.waterworld.WaterWorld;
+import nl.han.ica.waterworld.tiles.BoardsTile;
+import processing.core.PVector;
+
+import java.util.List;
+
+public class Docent extends AnimatedSpriteObject implements ICollidableWithTiles {
+
+    final int size = 25;
+    private final ICAman world;
+
+    /**
+     * Constructor
+     *
+     * @param world Referentie naar de wereld
+     */
+    public Docent(ICAman world) {
+        super(new Sprite("nl/han/ica/ICAman/media/original_pacman.png"), 1);
+        this.world = world;
+        setCurrentFrameIndex(0);
+        setWidth(size);
+        setHeight(size);
+    }
+
+    @Override
+    public void update() {
+        if (getX()<=0) {
+            setxSpeed(0);
+            setX(0);
+        }
+        if (getY()<=0) {
+            setySpeed(0);
+            setY(0);
+        }
+        if (getX()>=world.getWidth()-size) {
+            setxSpeed(0);
+            setX(world.getWidth() - size);
+        }
+        if (getY()>=world.getHeight()-size) {
+            setySpeed(0);
+            setY(world.getHeight() - size);
+        }
+
+    }
+
+    @Override
+    public void keyPressed(int keyCode, char key) {
+        final int speed = 5;
+        if (keyCode == world.LEFT) {
+            setDirectionSpeed(270, speed);
+            setCurrentFrameIndex(0);
+        }
+        if (keyCode == world.UP) {
+            setDirectionSpeed(0, speed);
+        }
+        if (keyCode == world.RIGHT) {
+            setDirectionSpeed(90, speed);
+            setCurrentFrameIndex(0);
+        }
+        if (keyCode == world.DOWN) {
+            setDirectionSpeed(180, speed);
+        }
+    }
+
+
+    @Override
+    public void tileCollisionOccurred(List<CollidedTile> collidedTiles)  {
+
+
+    }
+}
